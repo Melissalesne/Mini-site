3f8c4c9c617b78343d4869da242a91cfc73e101e
Melissa Lesne <melissalesne@orange.fr>
2021-01-05 15:58:12 +0100

    -

--- app/compile.php
+++ app/compile.php
@@ -29,33 +29,50 @@ $controller_file        = null;         // homepage
 $controller_path        = null;         // "../src/controllers/".$controller_file.".php";
 $controller_methode     = null;         // homepage_index
 
+// Convertion de la chaine $controller_expression en tableau
+// $controller_array[0]; // homepage
+// $controller_array[1]; // index ou null
+$controller_array = explode(":", $controller_expression);
 
-//////
 
 
+// -- Définition du fichier controleur ($controller_file = "homepage")
+$controller_file = $controller_array[0];
+$controller_path = "../src/controllers/".$controller_file.".php";
 
-echo "<pre>";
-print_r($route);
-echo "</pre>";
+// -- Définition de la fonction à exécuter
+$controller_methode = isset($controller_array[1]) ? $controller_array[1] : null;
 
-echo "<h3>controller_expression</h3>";
-echo "<pre>";
-print_r($controller_expression);
-echo "</pre>";
 
-echo "<h3>controller_file</h3>";
-echo "<pre>";
-print_r($controller_file);
-echo "</pre>";
+if ($controller_methode !== null && !empty($controller_methode)) 
+{
+    $controller_methode = $controller_file."_".$controller_methode;
+}
+else {
+    $controller_methode = $controller_file."_index";
+}
 
-echo "<h3>controller_path</h3>";
-echo "<pre>";
-print_r($controller_path);
-echo "</pre>";
 
-echo "<h3>controller_methode</h3>";
-echo "<pre>";
-print_r($controller_methode);
-echo "</pre>";
+/**
+ * Intégration du fichier controleur ($controller_path)
+ */
+if (!file_exists( $controller_path ))
+{
+    throw new Exception("Le fichier controleur de la route \"".$route[0]."\" est manquant.");
+}
 
-echo "<hr>";
+require_once $controller_path;
+
+
+/**
+ * Exécution de la fonction du controleur ($controller_methode)
+ */
+
+// test l'existance de la fonction du controleur
+if (!function_exists($controller_methode))
+{
+    throw new Exception("La methode \"".$controller_methode."\" de la route \"".$route[0]."\" est manquante.");
+}
+
+// Exécution de la fonction liée à la route
+$controller_methode();
